2bfabea303dc3ddb31376cb62164a94ee7d2f3db
Vitaliy <dvwhitem@gmail.com>
2024-08-11 21:48:15 +0300

    polish

--- ch-01/src/Ch01.java
+++ ch-01/src/Ch01.java
@@ -1,5 +1,13 @@
+import java.util.function.Function;
+
 public class Ch01 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Function<String, Integer> fnString = String::length;
+
+        //var n = null;
+        var n = "";
+
+        System.out.println("Fn: " + String.format(n));
     }
 }
