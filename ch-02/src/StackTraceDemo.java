59c9be17b8d0f5ca869c7d2f86b59375d062ded1
Vitaliy <dvwhitem@gmail.com>
2024-08-17 22:56:49 +0300

    start chapter 2

--- ch-02/src/StackTraceDemo.java
+++ ch-02/src/StackTraceDemo.java
@@ -1,5 +1,5 @@
-public class Main {
+public class StackTraceDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var i = Integer.parseInt("Error");
     }
 }
